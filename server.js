d8cb0bfcb64b062f3ab54e1af450dd2ee7dfd1c8
tcultivator <whypanahon06@gmail.com>
2025-06-19 09:38:39 +0800

    Update server.js

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.use(cors({
 app.use(cookieparser())
 const port = 8080;
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port:process.env.DB_PORT
-})
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
 db.connect((err) => {
     if (err) {
