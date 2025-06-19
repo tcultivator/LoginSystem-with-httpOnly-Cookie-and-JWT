11d1e61f1278497800efc3522ff1f3891d650140
tcultivator <whypanahon06@gmail.com>
2025-06-19 18:22:48 +0800

    Update landingPage.js

--- landingPage.js
+++ landingPage.js
@@ -59,3 +59,61 @@ function logout() {
 function cancelLogout() {
     document.getElementById('modalBody').style = `display:none;`
 }
+
+
+document.getElementById('depositBtn').addEventListener('click', () => {
+    document.getElementById('Deposit').style = `display:block;`
+})
+
+document.getElementById('withdrawBtn').addEventListener('click', () => {
+    document.getElementById('Withdraw').style = `display:block;`
+})
+
+
+const depositinput = document.getElementById('depositinput');
+async function confirmDeposit() {
+    console.log('na click ung deposit')
+    const deposit = await fetch('http://127.0.0.1:8080/deposit', {
+        method: 'PUT',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ depositAmmount: parseInt(accountBalance) + parseInt(depositinput.value) })
+    })
+
+    const data = await deposit.json()
+    if (deposit.ok) {
+        console.log('success')
+        getMeData()
+    }
+    else {
+        console.log('error')
+    }
+}
+
+
+
+
+
+const withdrawInput = document.getElementById('withdrawinput');
+async function confirmWithdraw() {
+    const withdraw = await fetch('http://127.0.0.1:8080/withdraw', {
+        method: 'PUT',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ withdrawAmmount: parseInt(accountBalance) - parseInt(withdrawInput.value) })
+    })
+
+    const data = await withdraw.json()
+    if (withdraw.ok) {
+        console.log('success')
+        getMeData()
+    }
+    else {
+        console.log('error')
+    }
+}
+
