7ea90b65ab745d5dfb4f1981aed6dda885235ab2
tcultivator <whypanahon06@gmail.com>
2025-06-20 12:49:26 +0800

    Update landingPage.js

--- landingPage.js
+++ landingPage.js
@@ -1,5 +1,8 @@
 let toggle = false;
 let accountBalance;
+let userId;
+let username;
+let transaction;
 document.getElementById('menuIcon').addEventListener('click', () => {
     toggle = !toggle;
     toggle ? (document.getElementById('rightSide').style = `right: 0;`) : (document.getElementById('rightSide').style = `right:-350px`)
@@ -15,8 +18,10 @@ async function getMeData() {
     const data = await getme.json()
     if (getme.ok) {
         document.getElementById('user').innerText = data.verifiedUserData.username;
-        document.getElementById('accountBalance').textContent = data.verifiedUserData.accountBalance;
+        document.getElementById('accountBalance').textContent = ` ₱ ${data.verifiedUserData.accountBalance}`;
         accountBalance = data.verifiedUserData.accountBalance;
+        userId = data.verifiedUserData.id;
+        username = data.verifiedUserData.username;
     }
     else {
         alert(data.message)
@@ -29,13 +34,13 @@ document.getElementById('user').addEventListener('click', () => {
     profileToggle = !profileToggle;
     profileToggle ? (document.getElementById('logout').style = `display:block`) : (document.getElementById('logout').style = `display:none`)
 })
-
 document.getElementById('logout').addEventListener('click', () => {
     profileToggle = !profileToggle;
     document.getElementById('logout').style = `display:none`
     document.getElementById('modalBody').style = `display:flex;`
 })
 function logout() {
+    console.log('test logout')
     document.getElementById('loadingBody').style = `display:flex;`
     document.getElementById('modalBody').style = `display:none;`
     setTimeout(() => {
@@ -45,18 +50,19 @@ function logout() {
 function cancelLogout() {
     document.getElementById('modalBody').style = `display:none;`
 }
+
 document.getElementById('depositBtn').addEventListener('click', () => {
     document.getElementById('Deposit').style = `display:block;`
     hideWithdraw()
 })
+
 document.getElementById('withdrawBtn').addEventListener('click', () => {
     document.getElementById('Withdraw').style = `display:block;`
     hideDeposit()
 })
 const depositinput = document.getElementById('depositinput');
 async function confirmDeposit() {
-    console.log('na click ung deposit')
-
+    let status;
     if (depositinput.value == '') {
         alert('Insert Ammount')
     }
@@ -70,26 +76,36 @@ async function confirmDeposit() {
             body: JSON.stringify({ depositAmmount: parseInt(accountBalance) + parseInt(depositinput.value) })
         })
         const data = await deposit.json()
+        const date = new Date();
         if (deposit.ok) {
+            transaction = `Deposit : ${parseInt(accountBalance)}₱ + ${parseInt(depositinput.value)}₱ `
+            status = 'Success';
             document.getElementById('notifDeposit').style = `display:flex;
             background-color: green;`
+            document.getElementById('message').textContent = data.message;
             setTimeout(() => {
                 getMeData()
                 hideDeposit()
                 document.getElementById('notifDeposit').style = `display:none;`
+                transactionHistory(userId, username, transaction, status, date)
             }, 1000);
         }
         else {
+            transaction = `Deposit : ${parseInt(accountBalance), '+', parseInt(depositinput.value)} `
+            status = 'Failed';
             document.getElementById('notifDeposit').style = `display:flex;
             background-color: red;`
+            document.getElementById('message').textContent = data.message;
             setTimeout(() => {
                 document.getElementById('notifDeposit').style = `display:none;`
+                transactionHistory(userId, username, transaction, status, date)
             }, 1000);
         }
     }
 }
 const withdrawInput = document.getElementById('withdrawinput');
 async function confirmWithdraw() {
+    let status;
     if (parseInt(accountBalance) < parseInt(withdrawInput.value)) {
         alert('insufficient balance ')
     }
@@ -104,21 +120,30 @@ async function confirmWithdraw() {
             },
             body: JSON.stringify({ withdrawAmmount: parseInt(accountBalance) - parseInt(withdrawInput.value) })
         })
+        const date = new Date();
         const data = await withdraw.json()
         if (withdraw.ok) {
+            transaction = `Withdraw : ${parseInt(accountBalance)}₱ - ${parseInt(withdrawInput.value)}₱ `
             document.getElementById('notifWithdraw').style = `display:flex;
             background-color: green;`
+            document.getElementById('message1').textContent = data.message;
+            status = 'Success';
             setTimeout(() => {
                 getMeData()
                 hideWithdraw()
                 document.getElementById('notifWithdraw').style = `display:none;`
+                transactionHistory(userId, username, transaction, status, date)
             }, 1000);
         }
         else {
-            document.getElementById('notif').style = `display:flex;
+            status = 'Failed';
+            transaction = `Withdraw : ${parseInt(accountBalance)}₱ - ${parseInt(withdrawInput.value)}₱ `
+            document.getElementById('notifWithdraw').style = `display:flex;
             background-color: red;`
+            document.getElementById('message1').textContent = data.message;
             setTimeout(() => {
-                document.getElementById('notif').style = `display:none;`
+                document.getElementById('notifWithdraw').style = `display:none;`
+                transactionHistory(userId, username, transaction, status, date)
             }, 1000);
         }
     }
@@ -131,3 +156,28 @@ function hideDeposit() {
     document.getElementById('Deposit').style = `display:none;`
     depositinput.value = ''
 }
+function transactionHistory(userId, username, transaction, status, date) {
+    let statusId = '';
+    if (status == 'Success') {
+        statusId = 'success'
+        console.log(statusId)
+    } else {
+        statusId = 'failed'
+    }
+    document.getElementById('transactionTable').innerHTML += ` 
+                        <td>${userId}</td>
+                        <td>${username}</td>
+                        <td>${transaction}</td>
+                        <td id='${statusId}'>${status}</td>
+                        <td>${date}</td>`
+}
+
+
+
+
+
+
+
+
+
+
